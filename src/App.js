ce1892d57ebfec7b3784033a14cd6fe32f355119
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-10-24 08:52:37 +0545

    fifth commit

--- src/App.js
+++ src/App.js
@@ -18,17 +18,12 @@ function App() {
     <>
     <Router>
       <Routes>
-        <Route path='/' element={<Explore />}/>
-        <Route path='/offers' element={<Offers />}/>
-        <Route path='/profile' element={<Profile />}/>
-        <Route path='/sign-in' element={<SignIn />}/>
-        <Route path='/sign-up' element={<SignUp />}/>
-        <Route path='/forgot-password' element={<ForgotPassword />}/>
-
-
-
-
-
+        <Route path='/' element={<Explore />} />
+        <Route path='/offers' element={<Offers />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
 
       </Routes>
       <Navbar/>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,95 @@
-
+import {useState} from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
 function SignUp() {
+
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    name:'',
+    email:'',
+    password:''
+  })
+
+  const { name, email, password } =formData
+  const navigate = useNavigate()
+
+  const onChange = (e) =>{
+    setFormData((prevState) =>({
+      ...prevState,
+      [e.target.id]: e.target.value, 
+    }))
+  }
   return (
-    <div> <h1>Sign Up</h1></div>
+    <>
+      <div className='pageContainer'> 
+        <header>
+          <p className='pageHeader'>
+            Welcome Back !!
+          </p>
+        </header>
+
+        <form>
+        <input
+            type='name'
+            className='nameInput'
+            placeholder='Name'
+            id='name'
+            value={name}
+            onChange={onChange}
+          />
+          <input
+            type='email'
+            className='emailInput'
+            placeholder='Email'
+            id='email'
+            value={email}
+            onChange={onChange}
+          />
+          <div className='passwordInputDiv'>
+            <input 
+              type={showPassword ? 'text' :'password'}
+              className='passwordInput'
+              placeholder='Password'
+              id='password'
+              value={password}
+              onChange={onChange}
+            />
+
+            <img 
+            src={visibilityIcon}
+            alt='show password' 
+            className='showPassword'
+            onClick={() => setShowPassword((prevState) =>
+            !prevState)}
+
+             />
+
+          </div>
+
+          <Link to='/forgot-password'
+          className='forgotPasswordLink'>
+            Forgot Password
+          </Link>
+
+          <div className='signInBar'>
+          <p className='signInText'>Sign In</p>
+          <button className='signInButton'>
+          <ArrowRightIcon fill='#ffffff' width='34px'
+          height='34px' />
+
+          </button>
+
+          </div>
+        </form>
+
+        <Link to='/sign-in'
+        className='registerLink'>
+        Sign In Instead
+        </Link>
+      </div>
+    </>
   )
 }
 
