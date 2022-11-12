c8dbb365decd4c6a5ee1c93ab7152fe156357d7b
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-11-12 13:33:11 +0545

    Building Website

---
+++ src/javascript/js/Tutorial21.js
@@ -0,0 +1,12 @@
+const { Icons } = require("react-toastify");
+
+console.log('Tutorail 21 exercise 2 JavaScript');
+
+
+// You have to create a dic and inject it inito the page which contains a HTMLHeadingElement
+
+
+
+// Whenever someone clicks on the Icons, it should be converted into the name of the subject
+
+
