1d643a75bbe7d35e7364cdc4b62b5aab242d66d8
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-11-13 16:31:29 +0545

    Project 1

---
+++ src/javascript/js/Project1/app.js
@@ -0,0 +1,10 @@
+console.log('Welcome to notes app. This is app.js')
+
+let addBtn = document.getElementById('addBtn');
+
+addBtn.addEventListener("click", function(e){
+    let addTxt = document.getElementById("addTxt");
+    let notes = localStorage.getItem("notes")
+
+    
+})
