5beb03053c3218e25f1ac4706e6ac7c2e9012742
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-11-14 13:17:06 +0545

    Keep Notes Project

--- src/javascript/js/Project1/app.js
+++ src/javascript/js/Project1/app.js
@@ -1,10 +1,77 @@
-console.log('Welcome to notes app. This is app.js')
+console.log("Welcome to notes app. This is app.js");
+showNotes();
+let addBtn = document.getElementById("addBtn");
 
-let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener("click", function (e) {
+  let addTxt = document.getElementById("addTxt");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.push(addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  //   console.log(notesObj);
+  showNotes();
+});
 
-addBtn.addEventListener("click", function(e){
-    let addTxt = document.getElementById("addTxt");
-    let notes = localStorage.getItem("notes")
+//Function to show element from local Storage
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
 
-    
-})
+  let html = "";
+  notesObj.forEach(function (element, index) {
+    html += `<div class="noteCard card my-2 mx-2" style="width: 18rem">
+        <div class="card-body">
+          <h5 class="card-title">Note ${index + 1}</h5>
+          <p class="card-text">${element + 1}
+          </p>
+          <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+        </div>
+      </div>`;
+  });
+
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  } else {
+    notesElm.innerHTML = `Nothing to show ! use "Add a Note" section above to add notes`;
+  }
+}
+
+//Function to delete a note
+
+function deleteNote(index) {
+  //   console.log("I am deleting", index);
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  showNotes();
+}
+
+let searchTxt = document.getElementById("searchTxt");
+searchTxt.addEventListener("input", function () {
+  let inputVal = search.value.toLowerCase();
+  // console.log('input event fired',inputVal)
+  let noteCards = document.getElementById("noteCard");
+  Array.from(noteCards).forEach(function (element) {
+    let cardTxt = element.getElementByTagName("p")[0].innerText;
+    if (cardTxt.includes(inputVal)) {
+      element.style.display = "block";
+    } else {
+      element.style.display = "None";
+    }
+  });
+});
