3d6208b1826955e4b0339aeeaf4e1f4cc6412b55
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-11-14 22:03:29 +0545

    math

---
+++ src/javascript/js/Tutorial23.js
@@ -0,0 +1,51 @@
+console.log("Welcome to tutorial number 23")
+
+
+let x = 34;
+let y = 8;
+
+let z;
+z= x-y;
+z= x+y;
+z=x*y;
+z=x/y;
+z = Math;
+z= Math.PI;
+z = Math.E;
+z= Math.round(5.5)
+z = Math.ceil(6.8)
+z = Math.ceil(2.1)
+z= Math.floor(9.1)
+z = Math.abs(-5)
+z = Math.sqrt(54)
+
+z= Math.pow(2,4)
+
+z= Math.min(2, 4, 5, 6, 74)
+z= Math.max(2, 4, 5, 6, 74)
+
+z = Math.random();
+
+z = 50*Math.random()
+z = Math.ceil(50 + (100-50)*Math.random())
+
+
+// a = (0,1)
+// a100 = a*100 = (0, 100)
+// a10_100 = 10+a*(100 - 10)
+
+console.log(z)
+232
+43
+6
+62
+2
+634735
+235
+2
+362
+62623
+252
+32623623523423-546
+-32
+033
