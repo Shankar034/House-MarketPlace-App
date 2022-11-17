e9cf046e135006e7f2372b47a3a0441047c5de90
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-11-17 14:42:04 +0545

    ES6 Classes

--- src/javascript/js/Tutorial31.js
+++ src/javascript/js/Tutorial31.js
@@ -23,30 +23,37 @@ class Employee{
 }
 
 
-class Programmer{
+class Programmer extends Employee{
 
-    constructor(givenName, givenExperience, givenDivision){
+   constructor(givenName, givenExperience, givenDivision, givenLanguage, github){
+    super(givenName, givenExperience, givenDivision);
+    this.language = givenLanguage;
+    this.github = github
+   }
 
-        this.name = givenName;
-        this.experience =givenExperience;
-        this.division =givenDivision;
+  favoriteLanguage(){
+    if(this.language == 'python'){
+        return 'Python'
+    }else{
+        return "Javascript"
     }
+   }
 
-    slogan(){
-        return `I am ${this.name} and this company is the best`;
-    }
-
-    joiningYear(){
-        return 2022 - this.experience;
-    }
-
-    static add(a, b){
-        return a + b;
-    }
+   static multiply(a, b){
+    return a +b;
+   }
 }
 
-harry = new Employee("Harry", 1, "Division");
-console.log(harry.joiningYear());
+// harry = new Employee("Harry", 1, "Division");
+// console.log(harry.joiningYear());
 
 
-console.log(Employee.add(43, 53))
+// console.log(Employee.add(43, 53))
+
+
+
+rohan = new Programmer("Rohan", 3, "lays", "go", "ROhan34");
+
+console.log(rohan)
+console.log(rohan.favoriteLanguage())
+console.log(Programmer.multiply(5, 7))
