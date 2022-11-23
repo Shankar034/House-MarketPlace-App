77852f28de9e65c1caf6f828316905ea03d225fd
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-11-23 16:26:55 +0545

    Project

--- src/javascript/js/Project2/index.js
+++ src/javascript/js/Project2/index.js
@@ -1,5 +1,3 @@
-
-
 console.log("This is index js");
 
 //Constructor
@@ -15,25 +13,47 @@ function Display() {}
 
 //Add methods to display prototype
 
-Display.prototype.add = function(book){
-    console.log('Adding to UI')
-    tableBody = document.getElementById('tableBody');
-    let uiString = `
+Display.prototype.add = function (book) {
+  console.log("Adding to UI");
+  tableBody = document.getElementById("tableBody");
+  let uiString = `
                 <tr>           
                     <td>${book.name}</td>
                     <td>${book.author}</td>
                     <td>${book.type}</td>
                 </tr>`;
-    tableBody.innerHTML += uiString;
-}
+  tableBody.innerHTML += uiString;
+};
 
-Display.prototype.clear = function(){   
-    let libraryForm= document.getElementById('libraryForm')
+//Implement the clear function
+Display.prototype.clear = function () {
+  let libraryForm = document.getElementById("libraryForm");
+  libraryForm.reset();
+};
 
-    libraryForm.reset();
+//Implement the validate function
+Display.prototype.validate = function (book) {
+  if (book.name.length < 3 || book.author.length < 2) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
-}
-
+Display.prototype.show = function (type, displaymessage) {
+  let message = document.getElementById("message");
+  message.innerHTML = `
+            <div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                    <strong>Message: </strong> ${displaymessage}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>`;
+            setTimeout(function()  {
+                message.innerHTML=''
+                
+            }, 2000);
+};
 
 //Add submit event listener to libraryForm
 
@@ -62,7 +82,14 @@ function libraryFormSubmit(e) {
   console.log(book);
 
   let display = new Display();
-  display.add(book);
-  display.clear();
-    e.preventDefault();
+  if (display.validate(book)) {
+    display.add(book);
+    display.clear();
+    display.show("Sucess !!", "Your book has been successfully added");
+  } else {
+    //Show error to the user
+    display.show("Danger", "Sorry you canot add this book");
+  }
+
+  e.preventDefault();
 }
