a3fa16a2b62636a22d542a54e7c4ffd99ce96059
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-11-25 15:59:47 +0545

    Using dictionary API

---
+++ src/javascript/js/Tutorial38.js
@@ -0,0 +1,6 @@
+console.log("This is Tutorial 38")
+
+
+//You have to pretend to use a word which will contain an object and you have to print the definition from all the results of that word API. You have to print it in the don .If you are using bootstrap then its a plus 
+
+
