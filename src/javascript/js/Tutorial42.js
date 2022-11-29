ed8602c75bd56fc95f1e9f8809339b50140b987d
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-11-29 10:11:45 +0545

    Fetch API

--- src/javascript/js/Tutorial42.js
+++ src/javascript/js/Tutorial42.js
@@ -7,19 +7,51 @@ let myBtn = document.getElementById("myBtn");
 //Div with id content
 let content = document.getElementById("content");
 
+// function getData(){
+//     console.log("Started getData");
+//     url = "shankar.txt";
+//     fetch(url).then((response)=>{
+//         console.log("Inside first then")
+//         return response.text();
+//     }).then((data)=>{
+//         console.log("Inside second then")
+//         console.log(data);
+//     })
+// }
+
+
 function getData(){
     console.log("Started getData");
-    url = "shankar.txt";
+    url = "https://api.github.com/users";
     fetch(url).then((response)=>{
         console.log("Inside first then")
-        return response.text();
+        return response.json();
     }).then((data)=>{
         console.log("Inside second then")
         console.log(data);
     })
 }
-console.log("Before running get data");
-getData()
-console.log("After running get data");
 
+function postData(){
+    
+    url = "https://dummy.restapiexample.com/api/v1/create";
+    data ='{"name":"Shankar034","salary":"123","age":"23"}';
+    params = {
+        method:"post",
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body:data
+    }
+    fetch(url, params).then((response)=>response.json())
+    .then((data)=>console.log(data))
+}
+
+
+// console.log("Before running get data");
+// getData()
+// console.log("After running get data");
+
+
+postData();
 
