f5e786d419051d500fc6381dc3dd7d03cf036f48
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-12-06 06:38:01 +0545

    Regular Expression

--- src/javascript/js/Tutorial48.js
+++ src/javascript/js/Tutorial48.js
@@ -12,11 +12,31 @@ console.log("This is Tutorial 48");
 
 let regex = /s[^h]ankar/;
 
-const str = "Shankar Bhai";
+
+
+
+
+//Quantifiers 
+//r can occur two times
+// regex = /hur{2}y/
+regex = /hur{0,2}y/
+
+// const str = "Shankar Bhai";
+
+
+
+// Groupings -we use paranthesis for grpupings 
+
+regex = /(ar){1}/
+regex = /(sha){1}([0-9]r){3}/
+
+str = "sha7r7r8r bhai hurry"
 
 
 let result = regex.exec(str);
 console.log("The result from exec is ", result);
+
+
 if(regex.test(str)){
     console.log(`The string ${str} matches the expression ${regex.source}`);
 }
