dca0a106c87ce3682162bab3e128293731fb8a54
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-12-13 16:29:50 +0545

    Project CV Scroller

---
+++ src/javascript/js/Project5/index.js
@@ -0,0 +1,15 @@
+console.log("This is Project CV scroller");
+
+
+//Data is an array of objects which contains information about the candidates
+const data = [
+        {
+            name: "Shankar Bhandari",
+            age: 24,
+            city:"Kanchanpur",
+            language: "JavaScript",
+            framework:"ReactJs",
+            image:''
+        }
+
+]
--- src/javascript/js/Tutorial54.js
+++ src/javascript/js/Tutorial54.js
@@ -2,16 +2,22 @@ console.log("This is Tutorial 54 file");
 
 const alarmSubmit = document.getElementById("alarmSubmit");
 
+
+//Add an event listener 
 alarmSubmit.addEventListener("click", setAlarm);
 
 var audio = new Audio(
   "https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3"
 );
-audio.play();
+
+//Function to play the alarm
 
 function ringBell() {
   audio.play();
 }
+
+
+//This function will run whenever alarm is set from UI
 
 function setAlarm(e) {
   e.preventDefault();
@@ -25,7 +31,8 @@ function setAlarm(e) {
 
   if (timeToAlarm >= 0) {
     setTimeout(() => {
+        console.log('Ringing Now...')
       ringBell();
-    }, timeout);
+    }, timeToAlarm);
   }
 }
