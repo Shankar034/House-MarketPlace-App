b3f6f2bce3d306431f949a2ae513e5c3acacca31
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-12-14 15:13:44 +0545

    JavaScript Maps

---
+++ src/javascript/js/Tutorial57.js
@@ -0,0 +1,38 @@
+console.log("This is Tutorial 57");
+
+// Maps in JavaScripts: We can use any type of key or value
+
+const myMap = new Map();
+
+const key1 = "myStr";
+const key2 = {};
+const key3 = function () {};
+
+//Setting Map Values
+
+myMap.set(key1, "This is a string");
+myMap.set(key2, "This is a blank obj");
+myMap.set(key3, "This is an empty function");
+
+console.log(myMap);
+
+//Getting the values from the map
+
+let value1 = myMap.get(key3);
+console.log(value1);
+
+//Get teh size of the map
+
+console.log(myMap.size);
+
+//You can loop using for-of to get keys and values
+
+for (let [key, value] of myMap) {
+  console.log(key, value);
+}
+
+
+//Get only Keys 
+for(let key of myMap.keys()){
+    console.log('key is ', key);
+}
