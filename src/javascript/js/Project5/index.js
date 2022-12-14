3d41f83a6b8a249932e8833575e2e236c8f42d9d
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-12-14 14:21:01 +0545

    Project CV Scroller

--- src/javascript/js/Project5/index.js
+++ src/javascript/js/Project5/index.js
@@ -48,3 +48,37 @@ function cvIterator(profiles) {
     },
   };
 }
+
+//Button listener for next button
+
+const candidates = cvIterator(data);
+nextCV();
+const next = document.getElementById("next");
+next.addEventListener("click", nextCV);
+
+function nextCV() {
+  const CurrentCandidate = candidates.next().value;
+  let image = document.getElementById("image");
+  let profile = document.getElementById("profile");
+
+
+  if(CurrentCandidate != undefined){
+
+      image.innerHTML = `<img src ='${CurrentCandidate.image}'>`;
+    
+    
+      profile.innerHTML = `<ul class="list-group">
+        
+        <li class="list-group-item">Name: ${CurrentCandidate.name}</li>
+        <li class="list-group-item">${CurrentCandidate.age} years old</li>
+        <li class="list-group-item">Lives in ${CurrentCandidate.city}</li>
+        <li class="list-group-item">Primarily works on ${CurrentCandidate.language}</li>
+        <li class="list-group-item">with ${CurrentCandidate.framework}</li>
+        
+      </ul>`;
+  }else
+  {
+    alert('End of candidate applications !!')
+    window.location.reload();
+  }
+}
