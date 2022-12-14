0a9b29bff81fd62542bd576814bb23942fae6709
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-12-14 15:46:37 +0545

    Sets in JavaScript

--- src/javascript/js/Tutorial58.js
+++ src/javascript/js/Tutorial58.js
@@ -17,3 +17,13 @@ mySet.add(true);
 mySet.add(34);
 
 console.log('The set looks this now:',mySet );
+
+let newSet = new Set([1, 34, 'This', false, {a:4, b:44}]);
+console.log('The set looks this now:',newSet );
+
+console.log(newSet.size);
+
+console.log(newSet.has(34));
+console.log('The set looks this now:',newSet );
+console.log(newSet.delete(1));
+console.log('The set looks this now:',newSet );
